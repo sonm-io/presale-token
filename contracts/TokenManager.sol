25216dec0f211c49a48d967d033d131e89dfadeb
Max Taldykin <jorpic@gmail.com>
2017-02-11 23:52:41 +0300

    Test proxy functions in TokenManager

--- contracts/TokenManager.sol
+++ contracts/TokenManager.sol
@@ -1,11 +1,73 @@
 pragma solidity ^0.4.4;
 
-import 'multisig-wallet/MultiSigWallet.sol';
+import "multisig-wallet/MultiSigWallet.sol";
+import "./PresaleToken.sol";
 
+
+
+// This extends MultiSigWallet with proxy functions to control PresaleToken
+// contract.
 
 contract TokenManager is MultiSigWallet {
+
     function TokenManager(address[] _owners, uint _required)
         MultiSigWallet(_owners, _required)
+    { }
+
+
+    event LogTokenSetPresalePhase(PresaleToken.Phase indexed _phase, uint _txId);
+    event LogTokenWithdrawEther(uint _txId);
+    event LogTokenSetCrowdsaleManager(address indexed _address, uint _txId);
+    event LogTokenSelfdestruct(uint _txId);
+
+
+    function tokenSetPresalePhase(
+        address _token,
+        PresaleToken.Phase _nextPhase
+    ) public
+        ownerExists(msg.sender)
     {
+        // bytes4(sha3('setPresalePhase(uint8)'))
+        bytes memory data =
+          hex"1ca2e94a0000000000000000000000000000000000000000000000000000000000000000";
+        data[35] = bytes1(uint8(_nextPhase));
+        uint txId = super.submitTransaction(_token, 0, data);
+        LogTokenSetPresalePhase(_nextPhase, txId);
+    }
+
+
+    function tokenWithdrawEther(address _token) public
+        ownerExists(msg.sender)
+    {
+        // bytes4(sha3('withdrawEther()'))
+        bytes memory data = hex"7362377b";
+        uint txId = super.submitTransaction(_token, 0, data);
+        LogTokenWithdrawEther(txId);
+    }
+
+
+    function tokenSetCrowdsaleManager(address _token, address _mgr) public
+        ownerExists(msg.sender)
+    {
+        // bytes4(sha3('setCrowdsaleManager(address)'))
+        bytes memory data =
+          hex"4defd1bf0000000000000000000000000000000000000000000000000000000000000000";
+
+        // assembly { mstore(add(data, 4), _mgr) }
+        for (uint i = 0; i < 20; i++) {
+          data[4+12+i] = byte(uint8(uint(_mgr) / (2**(8*(19 - i)))));
+        }
+
+        uint txId = super.submitTransaction(_token, 0, data);
+        LogTokenSetCrowdsaleManager(_mgr, txId);
+    }
+
+
+    function tokenSelfdestruct(address _token) public
+        ownerExists(msg.sender)
+    {
+        bytes memory data = hex"679d38e0";
+        uint txId = super.submitTransaction(_token, 0, data);
+        LogTokenSelfdestruct(txId);
     }
 }
--- test/TestTokenManager.js
+++ test/TestTokenManager.js
@@ -1,9 +1,64 @@
 const TokenManager = artifacts.require("./TokenManager.sol");
+const PresaleToken = artifacts.require("./PresaleToken.sol");
 
 contract("TokenManager", () => {
-  it("should create Tokenmanager with 3 members", () => {
-    TokenManager.deployed()
-      .then(mgr => assert.equal(mgr.getOwners().length, 3,
-        "has invalid number of members"));
-  })
+  const [a, b, c] = web3.eth.accounts;
+  let manager;
+  let token;
+
+  it("should be able to create TokenManager with 3 members", () =>
+    TokenManager.new([a, b, c], 2).then(mgr => {
+      manager = mgr;
+      return mgr.getOwners.call().then(owners =>
+        assert.equal(3, owners.length, "has invalid number of members"))
+    })
+  )
+
+  it("should be able to create PresaleToken with specified manager", () =>
+    PresaleToken.new(manager.address).then(tok => token = tok)
+  )
+
+  it("should be able to switch presale phase", () =>
+    manager.tokenSetPresalePhase(token.address, 1).then(res => {
+      const log = res.logs.find(log => log.event == "LogTokenSetPresalePhase");
+      const txId = log.args._txId;
+      return manager.confirmTransaction(txId, {from: b}).then(res =>
+        token.currentPhase.call().then(phase =>
+          assert.equal(1, phase.toFixed(), "should be in Running phase"))
+      )
+    })
+  )
+
+  it("should be able to set crowdsale manager", () =>
+    manager.tokenSetCrowdsaleManager(token.address, c).then(res => {
+      const log = res.logs.find(log => log.event == "LogTokenSetCrowdsaleManager");
+      const txId = log.args._txId;
+      return manager.confirmTransaction(txId, {from: b}).then(res =>
+        token.crowdsaleManager.call().then(csm =>
+          assert.equal(c, csm, "should change crowdsale manager address")))
+    })
+  )
+
+  it("should be able to withdraw funds", () =>
+    token.buyTokens({value: 3})
+      .then(() => {
+        const balance = web3.eth.getBalance(manager.address);
+        return assert.equal(0, balance.toFixed(), "token manager has no ether initially");
+      })
+      .then(() => manager.tokenWithdrawEther(token.address))
+      .then(res => {
+        const log = res.logs.find(log => log.event == "LogTokenWithdrawEther");
+        const txId = log.args._txId;
+        return manager.confirmTransaction(txId, {from: b}).then(res => {
+          const balance = web3.eth.getBalance(manager.address);
+          return assert.isAbove(balance.toFixed(), 0, "token manager got some ether");
+        })
+      })
+  )
+
+  // TODO:
+  // - move to migration phase
+  // - burn all tokens {from: c}
+  // - suicide
+
 })
