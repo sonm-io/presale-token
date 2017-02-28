f87b715757096461309e534acd600490e181b1cd
Max Taldykin <jorpic@gmail.com>
2017-02-28 23:54:06 +0300

    Add missing file: web-ui/src/TokenEvents.js

---
+++ web-ui/src/TokenEvents.js
@@ -0,0 +1,18 @@
+import React                from 'react';
+import List                 from 'material-ui/List/List';
+import ListItem             from 'material-ui/List/ListItem';
+import Subheader            from 'material-ui/Subheader';
+
+
+export default function (props) {
+  return (
+    <List>
+      { props.events.map((e, i) =>
+        <ListItem key={i} disabled={true}
+          primaryText={`${e.event} at block ${e.blockNumber}`}
+          secondaryText={JSON.stringify(e.args)}
+        />
+      )}
+    </List>
+  );
+}
